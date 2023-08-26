90b443f273c56c91fc8ac04d15614e1d97af1cfb
sebas-777 <sebastiancast06@hotmail.com>
2023-08-25 21:45:57 -0500

    Funciones elementales del ERC-20

--- 1_ERC-20.sol
+++ 1_ERC-20.sol
@@ -17,10 +17,93 @@ interface IERC20 {
     function transferFrom(
         address from,
         address to,
-        address amount
+        uint56 amount
     ) external returns (bool);
 
     // evento
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(address indexed  owner,address indexed  spender, uint256 value);
+}
+
+contract ERC20 is IERC20 { 
+
+    mapping(address => uint256) private _balances;
+    mapping(address => mapping(address => uint256)) private _allowances;
+
+    uint256 private _totalSupply;
+    string private _name;
+    string private _symbol;
+
+    constructor(string memory name_,string memory symbol_) {
+        _name = name_;
+        _symbol = symbol_;
+    } 
+
+    function name() public view virtual  returns (string memory){
+        return _name;
+    } 
+
+    function symbol() public view virtual  returns (string memory){
+        return _symbol;
+    } 
+
+    //505 -> 5.05(505 / 10**2)
+    function decimals() public view virtual returns(uint8) {
+        return 18;
+    } 
+
+    function totalSupply() public view virtual  override  returns (uint256) {
+        return _totalSupply;
+    } 
+
+    function balaceOf(address account) public view virtual  override  returns (uint256) {
+        return _balances[account];
+    }
+
+    function transfer() public virtual override returns(bool) {
+        address owner = msg.sender;
+        _transfer(owner,to,amount);
+        return  true;
+    } 
+
+    function allowance(address owner,address spender) public view virtual override returns (uint256) {
+       return  _allowances[owner][spender];
+    } 
+
+    function approve(address spender, uint256 amount) public virtual override returns (bool) {
+        address owner = msg.sender;
+        _approve(owner,spender,amount);
+        return true;
+    } 
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount) public override returns (bool){ 
+            address spender = msg.sender;
+            _spendAllowance(from,spender,amount);
+            _transfer(from, to , amount);
+            return true;
+
+        } 
+
+    function increaseAllwance( address spender, uint256 addedValue) public virtual returns (bool){
+        address owner = msg.sender;
+        _approve(owner,spender, _allowances[owner][spender] + addedValue);
+        return true;
+    } 
+
+    function decreaseAllowance(address spender,uint256 subtractedValue) public virtual returns (bool) {
+        address owner = msg.sender;
+        uint256 currentAllowance = _allowances[owner][spender];
+        require(currentAllowance >= subtractedValue,"ERC20: decreased allowance below zero");
+        unchecked{
+            _approve(owner,spender,currentAllowance-subtractedValue);
+        } 
+        return true;
+    }
+
+
+
+
 }
